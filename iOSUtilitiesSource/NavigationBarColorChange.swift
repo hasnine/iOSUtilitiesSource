a1580b6c91288c71273d2fb619ff6abe49231e8f
Jamil Hasnine Tamim <jamil.hasnine@sslwireless.com>
2018-12-20 13:56:14 +0600

    Changing navigation bar color in functionality added

---
+++ iOSUtilitiesSource/NavigationBarColorChange.swift
@@ -0,0 +1,23 @@
+//
+//  NavigationBarColorChange.swift
+//  iOSUtilitiesSource
+//
+//  Created by Jamil Hasnine Tamim on 20/12/18.
+//  Copyright © 2018 SSL Wireless Limited. All rights reserved.
+//
+
+import Foundation
+
+public class NavigationBarColorChange {
+    
+    private init (){
+        
+    }
+    
+   public static func coloredNavigation(barTintColor: UIColor, tintColor: UIColor,forgroundColor: UIColor, isTranslucent: Bool){
+        UINavigationBar.appearance().barTintColor = barTintColor
+        UINavigationBar.appearance().tintColor = tintColor
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: forgroundColor]
+        UINavigationBar.appearance().isTranslucent = isTranslucent
+    }
+}
