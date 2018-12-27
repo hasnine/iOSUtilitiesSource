65e54070c51bcdabf223187570f360296f19fce6
Jamil Hasnine Tamim <jamil.hasnine@sslwireless.com>
2018-12-27 12:33:08 +0600

    Keyboard show hide with view up-down

---
+++ iOSUtilitiesSource/iOSKeyboardShared.swift
@@ -0,0 +1,57 @@
+//
+//  iOSKeyboardShared.swift
+//  TestAnother
+//
+//  Created by Jamil Hasnine Tamim on 27/12/18.
+//  Copyright © 2018 SSL Wireless Limited. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class iOSKeyboardShared {
+    
+    static let shared = iOSKeyboardShared()
+    var isKeyboardAppear = false
+    var view: UIView?
+    
+    func keyBoardShowHide(view : UIView){
+        self.view = view
+        hideKeyboardWhenTappedAround()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if !isKeyboardAppear {
+            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                if self.view?.frame.origin.y == 0{
+                    self.view?.frame.origin.y -= keyboardSize.height/3
+                }
+            }
+            isKeyboardAppear = true
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification,view: UIView) {
+        if isKeyboardAppear {
+            if ((notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+                if self.view?.frame.origin.y != 0{
+                    self.view?.frame.origin.y = 0
+                }
+            }
+            isKeyboardAppear = false
+        }
+    }
+    
+    public func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view!.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view!.endEditing(true)
+    }
+    
+}
